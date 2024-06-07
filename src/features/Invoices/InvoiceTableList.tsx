149025e66bcd1a9b87be5a81a76890d2f6b54f1e
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2024-06-07 18:03:17 +0530

    added product and customer create form

--- src/features/Invoices/InvoiceTableList.tsx
+++ src/features/Invoices/InvoiceTableList.tsx
@@ -12,7 +12,7 @@ const fontWeight = "500";
 const HeaderStyle = (params: GridColumnHeaderParams) => {
 	return (
 		<Box sx={{ display: "flex", alignItems: "center" }}>
-			<Typography variant="h6" color="#000">
+			<Typography variant="h6" color="secondary.dark">
 				{params.colDef.headerName}
 			</Typography>
 		</Box>
