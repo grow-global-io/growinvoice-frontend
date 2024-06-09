9700f8f7b591788f1233a415cff2965323a3236a
somashekar <93281123+somashekar4138@users.noreply.github.com>
2024-06-10 02:31:50 +0530

    refactor: Remove duplicate import and unused code in CreateProduct.tsx

--- src/features/Products/CreateProduct.tsx
+++ src/features/Products/CreateProduct.tsx
@@ -1,20 +1,5 @@
 import { Drawer, Button } from "@mui/material";
-import { Drawer, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import ProductForm from "./ProductForm";
-import { useCreateProductStore } from "@store/createProductStore";
-
-export const ProductDrawer = ({
-	open,
-	handleClose,
-}: {
-	open: boolean;
-	handleClose: () => void;
-}) => (
-	<Drawer anchor="right" open={open} onClose={handleClose}>
-		<ProductForm />
-	</Drawer>
-);
 import ProductForm from "./ProductForm";
 import { useCreateProductStore } from "@store/createProductStore";
 
@@ -31,7 +16,6 @@ export const ProductDrawer = ({
 );
 
 export default function CreateProduct() {
-	const { setOpenProductForm } = useCreateProductStore.getState();
 	const { setOpenProductForm } = useCreateProductStore.getState();
 	return (
 		<>
