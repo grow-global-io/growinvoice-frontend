4c7a156b8196bcfc7987ce23a5e0f267db7018ad
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2024-06-25 17:23:54 +0530

    fixed minor changes

--- src/features/Settings/ParentofSidebar.tsx
+++ src/features/Settings/ParentofSidebar.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 const ParentofSidebar = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div>
-			<Sidebar children={children} />
+			<Sidebar>{children}</Sidebar>
 		</div>
 	);
 };
